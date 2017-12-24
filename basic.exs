60852c71056930de61dbea32af0f2dff4b5a7ce7
Vladimir Kivernik <vladimir@kivernik.ru>
2017-12-24 13:44:13 +0500

    Completed basic types and operators

--- basic.exs
+++ basic.exs
@@ -83,3 +83,47 @@ IO.inspect list ++ [0] # Дорогая (линейная сложность)
 # length - операция за линейное время
 
 # Ещё есть типы Port, Reference, и PID
+
+# +++ и -- для манипуляций со списками
+IO.inspect [1, 2, 3] ++ [4, 5, 6] # [1, 2, 3, 4, 5, 6]
+IO.inspect [1, 2, 3] -- [2] # [1, 3]
+
+IO.puts "foo" <> "bar" # конкатенация строк
+
+# В эликсире есть 3 булевых оператора: or, and и not
+IO.puts false or is_atom(:example) # true
+
+# Булевы операторы требуют тип boolean в качестве операндов, иначе ошибка
+# iex> 1 and true
+# ** (BadBooleanError) expected a boolean on left-side of "and", got: 1
+
+# Операторы ||, && и ! принимают аргуенты любого типа
+# Все значения кроме false и nil считаются true
+
+# or
+IO.puts 1 || true # 1
+IO.puts false || 11 # 11
+
+# and
+IO.puts nil && 13 # nil
+IO.puts true && 17 # 17
+
+# !
+IO.puts !true # false
+IO.puts !1 # false
+IO.puts !nil # true
+
+# Операторы сравнения ==, !=, ===, !==, <=, >=, <, >
+IO.puts 1 == 1 # true
+IO.puts 1 != 2 # true
+IO.puts 1 < 2 # true
+
+# Разница между == и === в более строгом стравнении типов integer и float
+IO.puts 1 == 1.0 # true
+IO.puts 1 === 1.0 # false
+
+# В эликсире мы можем сравнить данные разных типов, существует определенный порядок
+# number < atom < reference < function < port < pid < tuple < map < list < bitstring
+# Сделано так из практических соображений, чтобы алгоритмы сортировки не беспокоились
+# о типах данных и в каком порядке их сортировать
+IO.puts 1 < :atom # true
